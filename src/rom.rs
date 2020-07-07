977a34cabade96b08c0d64c5937629d1e15bd3d4
Robert Kalmar <rkalmar@cloudera.com>
2020-07-07 15:21:45 +0200

    Fix ROM member names

--- src/rom.rs
+++ src/rom.rs
@@ -5,9 +5,11 @@ const CRH_PAGE_SIZE: usize = 8 * 1024;
 pub struct ROM {
     prg_rom_page_count: u8,
     chr_rom_page_count: u8,
-    mapper_id: u8,
+    flags_6: u8,
+    flags_7: u8,
     prg_ram_page_count: u8,
-    flags: u8,
+    flags_9: u8,
+    flags_10: u8,
     prg_rom: Vec<u8>,
     chr_rom: Vec<u8>,
 }
@@ -15,16 +17,18 @@ pub struct ROM {
 impl ROM {
     pub fn from_slice(value: &[u8]) -> Result<Self, &'static str> {
         match value {
-            [0x4E, 0x45, 0x53, 0x1A, prg_rom_page_count, chr_rom_page_count, mapper_low, mapper_high, prg_ram_page_count, flags, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, rest @ ..] =>
+            [0x4E, 0x45, 0x53, 0x1A, prg_rom_page_count, chr_rom_page_count, flags_6, flags_7, prg_ram_page_count, flags_9, flags_10, 0x00, 0x00, 0x00, 0x00, 0x00, rest @ ..] =>
             {
                 let prg_rom_size = *prg_rom_page_count as usize * PRG_PAGE_SIZE;
                 let chr_rom_size = *chr_rom_page_count as usize * CRH_PAGE_SIZE;
                 Ok(Self {
                     prg_rom_page_count: *prg_rom_page_count,
                     chr_rom_page_count: *chr_rom_page_count,
-                    mapper_id: mapper_low | (mapper_high << 4),
+                    flags_6: *flags_6,
+                    flags_7: *flags_7,
                     prg_ram_page_count: *prg_ram_page_count,
-                    flags: *flags,
+                    flags_9: *flags_9,
+                    flags_10: *flags_10,
                     prg_rom: rest[0..prg_rom_size].to_vec(),
                     chr_rom: rest[prg_rom_size..prg_rom_size + chr_rom_size].to_vec(),
                 })
