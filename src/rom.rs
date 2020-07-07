0c3c7360f4c82ce5b27ab95ec2f0b75a02330968
Robert Kalmar <rkalmar@cloudera.com>
2020-07-07 18:18:25 +0200

    ROM takes reference to slices instead of creating a Vec

--- src/rom.rs
+++ src/rom.rs
@@ -2,7 +2,7 @@ const PRG_PAGE_SIZE: usize = 16 * 1024;
 const CRH_PAGE_SIZE: usize = 8 * 1024;
 
 #[derive(Debug, Default)]
-pub struct ROM {
+pub struct ROM<'a> {
     prg_rom_page_count: u8,
     chr_rom_page_count: u8,
     flags_6: u8,
@@ -10,12 +10,12 @@ pub struct ROM {
     prg_ram_page_count: u8,
     flags_9: u8,
     flags_10: u8,
-    prg_rom: Vec<u8>,
-    chr_rom: Vec<u8>,
+    prg_rom: &'a [u8],
+    chr_rom: &'a [u8],
 }
 
-impl ROM {
-    pub fn from_slice(value: &[u8]) -> Result<Self, &'static str> {
+impl<'a> ROM<'a> {
+    pub fn from_slice(value: &'a [u8]) -> Result<Self, &'static str> {
         match value {
             [0x4E, 0x45, 0x53, 0x1A, prg_rom_page_count, chr_rom_page_count, flags_6, flags_7, prg_ram_page_count, flags_9, flags_10, 0x00, 0x00, 0x00, 0x00, 0x00, rest @ ..] =>
             {
@@ -29,11 +29,23 @@ impl ROM {
                     prg_ram_page_count: *prg_ram_page_count,
                     flags_9: *flags_9,
                     flags_10: *flags_10,
-                    prg_rom: rest[0..prg_rom_size].to_vec(),
-                    chr_rom: rest[prg_rom_size..prg_rom_size + chr_rom_size].to_vec(),
+                    prg_rom: &rest[0..prg_rom_size],
+                    chr_rom: &rest[prg_rom_size..prg_rom_size + chr_rom_size],
                 })
             }
             _ => Err("Input is not a valid iNES file format"),
         }
     }
+
+    pub fn mirroring(&self) -> Mirroring {
+        match (self.flags_6 & 0x01) != 0 {
+            false => Mirroring::Horizontal,
+            true => Mirroring::Vertical,
+        }
+    }
+}
+
+pub enum Mirroring {
+    Horizontal,
+    Vertical,
 }
